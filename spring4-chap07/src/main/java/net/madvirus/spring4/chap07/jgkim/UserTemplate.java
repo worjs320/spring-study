eb61abe564e3c6b1c1e9d6ad3684a8dd1905e929
worjs320 <worjs320@naver.com>
2021-07-07 23:00:32 +0900

    test: apply lombok

--- spring4-chap07/src/main/java/net/madvirus/spring4/chap07/jgkim/UserTemplate.java
+++ spring4-chap07/src/main/java/net/madvirus/spring4/chap07/jgkim/UserTemplate.java
@@ -1,40 +1,13 @@
 package net.madvirus.spring4.chap07.jgkim;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserTemplate {
     private String id;
     private String password;
     private String name;
     private String email;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
