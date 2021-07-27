7499be71252a220615011841cecc3be70be4fe5b
worjs320 <worjs320@naver.com>
2021-07-28 00:57:29 +0900

    test: ResponseStatus test

--- spring4-chap07/src/main/java/net/madvirus/spring4/chap07/jgkim/CommonException.java
+++ spring4-chap07/src/main/java/net/madvirus/spring4/chap07/jgkim/CommonException.java
@@ -1,11 +1,15 @@
 package net.madvirus.spring4.chap07.jgkim;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice("net.madvirus.spring4.chap07.jgkim")
 public class CommonException {
     @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
     public String ExceptionHandler() {
         return "error";
     }
