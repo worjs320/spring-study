65358825047105be914680610fa2f00df9bf092a
worjs320 <worjs320@naver.com>
2021-08-01 18:25:28 +0900

    test: AbstractExcelView to AbstractXlsxView migration work

--- spring4-chap08/src/main/java/net/madvirus/spring4/chap08/stat/PageRankView.java
+++ spring4-chap08/src/main/java/net/madvirus/spring4/chap08/stat/PageRankView.java
@@ -6,22 +6,19 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.springframework.web.servlet.view.document.AbstractExcelView;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
-public class PageRankView extends AbstractExcelView {
-
+public class PageRankView extends AbstractXlsxView {
 	@SuppressWarnings("unchecked")
 	@Override
-	protected void buildExcelDocument(Map<String, Object> model,
-			HSSFWorkbook workbook, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		response.setHeader("Content-Disposition", "attachment; filename=\"pagerank.xls\";");
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+		httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"pagerank.xlsx\";");
 
-		HSSFSheet sheet = createFirstSheet(workbook);
+		Sheet sheet = createFirstSheet(workbook);
 		createColumnLabel(sheet);
 
 		List<PageRank> pageRanks = (List<PageRank>) model.get("pageRankList");
@@ -31,26 +28,25 @@ public class PageRankView extends AbstractExcelView {
 		}
 	}
 
-	private HSSFSheet createFirstSheet(HSSFWorkbook workbook) {
-		HSSFSheet sheet = workbook.createSheet();
+	private Sheet createFirstSheet(Workbook workbook) {
+		Sheet sheet = workbook.createSheet();
 		workbook.setSheetName(0, "페이지 순위");
 		sheet.setColumnWidth(1, 256 * 20);
 		return sheet;
 	}
 
-	private void createColumnLabel(HSSFSheet sheet) {
-		HSSFRow firstRow = sheet.createRow(0);
-		HSSFCell cell = firstRow.createCell(0);
+	private void createColumnLabel(Sheet sheet) {
+		Row firstRow = sheet.createRow(0);
+		Cell cell = firstRow.createCell(0);
 		cell.setCellValue("순위");
 
 		cell = firstRow.createCell(1);
 		cell.setCellValue("페이지");
 	}
 
-	private void createPageRankRow(HSSFSheet sheet, PageRank rank,
-			int rowNum) {
-		HSSFRow row = sheet.createRow(rowNum);
-		HSSFCell cell = row.createCell(0);
+	private void createPageRankRow(Sheet sheet, PageRank rank, int rowNum) {
+		Row row = sheet.createRow(rowNum);
+		Cell cell = row.createCell(0);
 		cell.setCellValue(rank.getRank());
 
 		cell = row.createCell(1);
