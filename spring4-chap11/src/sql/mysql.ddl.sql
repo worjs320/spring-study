49eb0f6fbdfd112198e0d804e0a89ad451885c97
worjs320 <worjs320@naver.com>
2021-08-21 17:29:04 +0900

    creat: create person scheme

--- spring4-chap11/src/sql/mysql.ddl.sql
+++ spring4-chap11/src/sql/mysql.ddl.sql
@@ -1,7 +1,17 @@
 --테이블 생성
-create table guestbook.guestmessage (
-	id int auto_increment primary key,
-	name varchar(100),
-	message text,
-	creationTime datetime
-) engine=InnoDB character set = utf8;
+create table guestmessage
+(
+    id           int auto_increment primary key,
+    name         varchar(100),
+    message      text,
+    creationTime datetime
+);
+
+create table person
+(
+    num    int auto_increment primary key,
+    name   varchar(100),
+    gender varchar(100),
+    age    number,
+    birth  datetime
+);
