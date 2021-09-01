1db661a16c9e0214865177d1043b6b3b4ff3cd1f
worjs320 <worjs320@naver.com>
2021-09-02 00:22:57 +0900

    ddl: ddl edit

--- spring4-chap13-hib/src/sql/h2.ddl.sql
+++ spring4-chap13-hib/src/sql/h2.ddl.sql
@@ -2,21 +2,21 @@
 create table ITEM (
 	ITEM_ID int auto_increment primary key,
 	PRICE INT
-) engine=InnoDB character set = utf8;
+);
 
 create table PAYMENT_INFO (
 	PAYMENT_INFO_ID int auto_increment primary key,
 	PRICE INT
-) engine=InnoDB character set = utf8;
+);
 
 create table PURCHASE_ORDER (
 	PURCHASE_ORDER_ID int auto_increment primary key,
 	ITEM_ID INT,
 	PAYMENT_INFO_ID INT,
 	ADDRESS varchar(255)
-)
+);
 
 insert into ITEM values (1, 15000);
 
-truncate PAYMENT_INFO;
-truncate PURCHASE_ORDER;
+truncate TABLE  PAYMENT_INFO;
+truncate TABLE PURCHASE_ORDER;
