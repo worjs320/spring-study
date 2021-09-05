200de8bd592de26b352d709b5de64f65b4767bdb
worjs320 <worjs320@naver.com>
2021-09-05 22:17:50 +0900

    feat: person entity add

---
+++ spring4-chap13-hib/src/main/java/net/madvirus/spring4/chap13/store/domain/Person.java
@@ -0,0 +1,53 @@
+package net.madvirus.spring4.chap13.store.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PERSON")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "AGE")
+    private Integer age;
+
+    @Column(name = "GENDER")
+    private String gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
