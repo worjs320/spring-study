45c5f77c1eaa6ed573af91be8f6025e74cdfb170
worjs320 <worjs320@naver.com>
2021-10-02 22:39:25 +0900

    feat: person entity add

---
+++ spring4-chap14/src/main/java/net/madvirus/spring4/chap14/domain/Person.java
@@ -0,0 +1,41 @@
+package net.madvirus.spring4.chap14.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "PERSON")
+@Getter
+@Setter
+public class Person {
+    @Id
+    @Column(name = "PERSON_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "GENDER")
+    private String gender;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "BIRTHDAY")
+    private Date BIRTHDAY;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", BIRTHDAY=" + BIRTHDAY +
+                '}';
+    }
+}
